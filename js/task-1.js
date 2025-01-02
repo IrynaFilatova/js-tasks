dc47e464946196217435d6d3ab7d50c7803e263c
irina <iryna.filatova@gmail.com>
2025-01-02 18:26:07 +0100

    js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,43 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  let totalPrice = quantity * pricePerDroid;
-  return totalPrice > customerCredits
-    ? `Insufficient funds!`
-    : `You ordered ${quantity} droids worth ${totalPrice}`;
-}
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+// Задачи на работу с объектами
+// 	1.	Напишите функцию getTotalPrice(products), которая принимает объект,
+//  где ключи — названия продуктов, а значения — их цены.
+// Функция должна возвращать сумму всех цен.
+// function getTotalPrice(products) {
+//   let totalPrices = 0;
+//   for (const value of Object.values(products)) {
+//     totalPrices += value;
+//   }
+//   return totalPrices;
+// }
+// console.log(getTotalPrice({ apple: 1, banana: 2, orange: 3 })); // 6
+// console.log(getTotalPrice({ coffee: 10, tea: 7 })); // 17
+
+// Создайте функцию filterByPrice(products, priceLimit), которая принимает
+//  объект продуктов с ценами и порог цены. Она возвращает новый объект,
+//  включающий только те продукты, которые стоят меньше заданного порога.
+
+// function filterByPrice(products, priceLimit) {
+//   let newObject = {};
+//   for (const key in products) {
+//     if (products[key] < priceLimit) {
+//       newObject[key] = products[key];
+//     }
+//   }
+//   return newObject;
+// }
+
+// console.log(filterByPrice({ apple: 1, banana: 2, orange: 3 }, 2)); // { apple: 1 }
+// console.log(filterByPrice({ coffee: 10, tea: 7 }, 8)); // { tea: 7 }
+
+// 3.	Напишите функцию countItems(products), которая принимает объект, где ключи — это продукты,
+//  а значения — их количество, и возвращает общее количество всех продуктов.
+
+// function countItems(products) {
+//   let totalProductObject = 0;
+//   for (const key in products) {
+//     totalProductObject += products[key];
+//   }
+//   return totalProductObject;
+// }
+// console.log(countItems({ apples: 2, bananas: 4, oranges: 3 })); // 9
+// console.log(countItems({ shirts: 5, pants: 3, hats: 2 })); // 10
--- js/task-2.js
+++ js/task-2.js
@@ -1,11 +1,17 @@
-function formatMessage(message, maxLength) {
-  return message.length <= maxLength
-    ? message
-    : message.slice(0, maxLength) + `...`;
+// function formatMessage(message, maxLength) {
+//   return message.length <= maxLength
+//     ? message
+//     : message.slice(0, maxLength) + `...`;
+// }
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+function greet(name) {
+  return `Welcome ${name}!`;
 }
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// Викликаємо функцію greet і виводимо результат у консоль
+console.log(`Welcome`); // "Welcome Mango!"
